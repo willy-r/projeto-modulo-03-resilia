450b3598aee5f9679858e034cc2592ed13caf323
William Rodrigues <williamrodrigues2442@gmail.com>
2021-10-24 21:13:15 -0300

    Adiciona README em consultas e consultas de William Rodrigues

---
+++ consultas/README.md
@@ -0,0 +1,5 @@
+# Consultas feitas pelo grupo 7
+
+Abaixo estão os gráficos de todas as perguntas feitas pelo grupo 7, assim como uma rápida explicação de cada um do porquê da pergunta. Mas basicamente focamos em responder perguntas que pessoas que não entendem muito dos jogos da NBA ou de basquete em si se fariam para entender o jogo melhor e a importância dos jogadores para a NBA.
+
+O tema foi basicamente fazer um "TOP 5" alguma coisa, focando principalmente nas temporadas de 2015 à 2019, que são contabilizadas como por exemplo 2015-16, pois a temporada começa em Outubro de um ano e termina em Abril do próximo ano.
--- consultas/william-consultas.sql
+++ consultas/william-consultas.sql
@@ -1,6 +1,31 @@
 --
--- Exemplo de consulta.
+-- Perguntas/consultas de William Rodrigues.
 --
 
--- Pergunta?
-SELECT * FROM `time`;
+-- Quais são os 5 times que mais ganharam jogos em casa?
+SELECT
+	CONCAT(t.cidade, ' ', t.nome) nome_time,
+  COUNT(*) qtd_vitorias
+FROM `time` t
+INNER JOIN jogo j
+	ON t.id_time = j.id_time_casa
+WHERE
+	j.temporada BETWEEN 2015 AND 2019
+  AND j.vitoria_time_casa = TRUE
+GROUP BY nome_time
+ORDER BY qtd_vitorias DESC
+LIMIT 5;
+
+-- Quais são os 5 times que mais ganharam jogos fora de casa?
+SELECT
+	CONCAT(t.cidade, ' ', t.nome) nome_time,
+  COUNT(*) qtd_vitorias
+FROM `time` t
+INNER JOIN jogo j
+	ON t.id_time = j.id_time_visitante
+WHERE
+	j.temporada BETWEEN 2015 AND 2019
+  AND j.vitoria_time_casa = FALSE
+GROUP BY nome_time
+ORDER BY qtd_vitorias DESC
+LIMIT 5;
